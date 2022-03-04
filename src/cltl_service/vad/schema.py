74151580d45824a0a9583c17b0ae8462f8b32ad1
Thomas Baier <tbaier@math.bme.hu>
2022-03-04 09:39:27 +0100

    Update imports to use emissor events from cltl.combot

--- src/cltl_service/vad/schema.py
+++ src/cltl_service/vad/schema.py
@@ -1,23 +1,8 @@
 import uuid
-
+from cltl.combot.event.emissor import AnnotationEvent
 from cltl.combot.infra.time_util import timestamp_now
 from dataclasses import dataclass
-
 from emissor.representation.scenario import Mention, Annotation
-
-
-# TODO move to cltl.combot
-@dataclass
-class MentionEvent:
-    type: str
-    mention: Mention
-
-
-@dataclass
-class VadMentionEvent(MentionEvent):
-    @classmethod
-    def create(cls, segment, annotation):
-        return cls(cls.__name__, Mention(str(uuid.uuid4()), [segment], [annotation]))
 
 
 @dataclass
@@ -25,3 +10,11 @@ class VadAnnotation(Annotation[float]):
     @classmethod
     def for_activation(cls, activation: float, source: str):
         return cls(cls.__name__, activation, source, timestamp_now())
+
+
+@dataclass
+class VadMentionEvent(AnnotationEvent[VadAnnotation]):
+    @classmethod
+    def create(cls, segment, annotation):
+        return cls(cls.__name__, [Mention(str(uuid.uuid4()), [segment], [annotation])])
+
