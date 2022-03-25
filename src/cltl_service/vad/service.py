a2ee3aeb9bc22644a68050dea49b33ff12077302
Thomas Baier <tbaier@math.bme.hu>
2022-03-25 09:29:19 +0100

    Set name in TopicWorker

--- src/cltl_service/vad/service.py
+++ src/cltl_service/vad/service.py
@@ -52,7 +52,8 @@ class VadService:
 
     def start(self, timeout=30):
         self._topic_worker = TopicWorker([self._mic_topic], self._event_bus, provides=[self._vad_topic],
-                                         resource_manager=self._resource_manager, processor=self._process)
+                                         resource_manager=self._resource_manager, processor=self._process,
+                                         name=self.__class__.__name__)
         self._topic_worker.start().wait()
         self._executor = ThreadPoolExecutor(max_workers=2)
 
